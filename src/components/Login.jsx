056a90bfb9a84e698b479979d2a467fb5d28c617
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-09-13 02:38:41 +0530

    form validation done'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
 import bg from '../assets/background.jpg'
+import { validateData } from '../utils/validate.js'
 
 const Login = () => {
 
-    const [signIn,setSignIn] = useState(true)
+    const [signIn,setSignIn] = useState(true);
+    const [errorMsg,setErrorMsg] = useState(null)
+
+    const email = useRef(null);
+    const password = useRef(null);
+
+
+    const handleButtonClick = () =>{
+      const message = validateData(email.current.value,password.current.value)
+       //console.log(email,password);
+       //console.log(password.current.value);
+       //console.log((message));
+       setErrorMsg(message);
+
+    }
 
     const toggleSignIn = () => {
         setSignIn(!signIn)
@@ -19,14 +34,31 @@ const Login = () => {
         
         </div>
 
-    <form action="" className='bg-black opacity-80 text-white w-1/4 absolute p-10 my-36 mx-auto right-0 left-0'>
+    <form onSubmit={(e)=>e.preventDefault()} className='bg-black opacity-80 text-white w-1/4 absolute p-10 my-36 mx-auto right-0 left-0'>
         <h1 className='text-3xl font-bold text-center py-4'>{signIn ? 'LogIn':'SignUp'}</h1>
         {!signIn && <input type="text" name="" id="" placeholder='Enter your name' className='w-full my-4 p-2 bg-slate-600' />}
-        <input type="email" name="" id="" placeholder='Email address' className='w-full my-4 p-2 bg-slate-600' />
-        <input type="password" name="" id="" placeholder='Password' className='w-full my-4 p-2 bg-slate-600'/>
-        <button className='w-full my-4 p-2 bg-red-700 rounded-xl'>{signIn ? 'LogIn':'SignUp'}</button>
+        
+        <input 
+         ref={email}
+         type="email"
+         placeholder='Email address' 
+         className='w-full my-4 p-2
+          bg-slate-600' />
+         
 
-        <p className='text-gray-400 py-4 cursor-pointer' onClick={toggleSignIn}>
+        <input 
+         ref={password}
+         type="password" 
+         placeholder='Password' 
+         className='w-full my-4 p-2 bg-slate-600'/>
+         <p className='text-red-600 font-bold text-xl'>{errorMsg}</p>
+
+        <button 
+        className='w-full my-4 p-2 bg-red-700 rounded-xl' 
+        onClick={handleButtonClick}>{signIn ? 'LogIn':'SignUp'}
+        </button>
+
+        <p className='text-gray-400 py-4 cursor-pointer' onClick={toggleSignIn} >
            {signIn?'Dont have an account? Sign Up now' : 
            'Already have an account? LogIn'} </p>
     </form>
